95ebf61376b4c3a1c8e8aa052a102dbeba40b039
nikita1017 <78160117+nikita1017@users.noreply.github.com>
2022-10-05 21:59:57 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from mcstatus import JavaServer
+# def players():
+#   from mcstatus import JavaServer
 
-
-# You can pass the same address you'd enter into the address field in minecraft into the 'lookup' function
-# If you know the host and port, you may skip this and use JavaServer("example.org", 1234)
-server = JavaServer.lookup("s6.joinserver.ru:25615")
-
-status = server.status()
-print(f"Сейчас играет на сервере: {status.players.online} Задержка сервера: {status.latency} ms")
-latency = server.ping()
-print(f"Задержка сервера: {latency} ms")
-globvar = 0
-def set_globvar_to_one():
-    global globvar    # Needed to modify global copy of globvar
-    globvar = 1
+#   # You can pass the same address you'd enter into the address field in minecraft into the 'lookup' function
+#   # If you know the host and port, you may skip this and use JavaServer("example.org", 1234)
+#   server = JavaServer.lookup("s6.joinserver.ru:25615")
+#   status = server.status()
+#   return status.players.online
---
+++ script.js
@@ -0,0 +1,6 @@
+let knopka=document.getElementsByClassName("knopka");
+let razdel=document.getElementsByClassName("razdel");
+
+function getScroll(){
+    console.log(razdel[0].offsetTop);
+}
